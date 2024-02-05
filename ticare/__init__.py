5017e4c7ebd63da10df816c0ffece7ec28fd2c4c
mariamMohammed0 <93684180+mariamMohammed0@users.noreply.github.com>
2024-02-05 08:03:12 +0200

    user login/register/sessions

--- ticare/__init__.py
+++ ticare/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 CORS(app)
 
 UPLOAD_FOLDER = 'savedVedio'
@@ -9,3 +12,15 @@ ALLOWED_EXTENSIONS = {'webm'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['COUNT'] = 1  # Initialize the counter for filenames
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+app.app_context().push()
+
+
+from ticare import routes
--- ticare/routes.py
+++ ticare/routes.py
@@ -1,14 +1,57 @@
-from flask import request, jsonify
-from ticare import app, ALLOWED_EXTENSIONS
-from functions import preprocesss
+from flask import request, jsonify ,render_template, redirect, url_for, flash
+from ticare import app, ALLOWED_EXTENSIONS , db
+from ticare.models import User ,Session
+from ticare.forms import RegisterForm, LoginForm
+from ticare.functions import preprocesss
+from flask_login import login_user
+
 import os
-
-
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
 
+@app.route('/sessions')
+def session_page():
+    Sessions = Session.query.all()
+    print(Sessions)
+    return render_template('sessions.html', Sessions=Sessions)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('home_page'))
+    if form.errors != {}: #If there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('home_page'))
+        else:
+            flash('Username and password are not match! Please try again', category='danger')
+
+    return render_template('login.html', form=form)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
